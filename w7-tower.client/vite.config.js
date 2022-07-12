53233bca243e130d03d76f42da6d723b1a14117d
Cameron <cameron.kueneman@gmail.com>
2022-07-12 09:46:44 -0600

    build

--- w7-tower.client/vite.config.js
+++ w7-tower.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../w7-tower/client',
     sourcemap: false
   },
   server: {
